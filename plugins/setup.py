eff8da75cc31abaca7c8c80ae8b74bc87353b785
marco.renzi <marcorenzi8@gmail.com>
2024-05-22 16:38:55 +0200

    first commit

---
+++ plugins/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+VERSION = "0.1.0"
+PACKAGE_NAME = "plugins"
+AUTHOR = "Valerio Luzzi, Marco Renzi"
+EMAIL = "valerio.luzzi@gecosistema.com, marco.renzi@gecosistema.com"
+GITHUB = "https://github.com/SaferPlaces2023/pygeoapi_ingestor_plugin"
+DESCRIPTION = "An utils functions package"
+
+setuptools.setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    license='MIT',
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    url=GITHUB,
+    packages=setuptools.find_packages("src"),
+    package_dir={'': 'src'},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["pygeoapi"]
+)
---
+++ plugins/src/plugins/ingestor_process.py
@@ -0,0 +1,376 @@
+# =================================================================
+#
+# Authors: Valerio Luzzi <valluzzi@gmail.com>
+#
+# Copyright (c) 2023 Valerio Luzzi
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
+# curl -X POST -H "Content-Type: application/json" -d "{\"inputs\":{\"name\":\"valerio\"}}" http://localhost:5000/processes/ingestor-process/execution
+# curl -X POST -H "Content-Type: application/json" -d "{\"inputs\":{\"name\":\"gdalinfo\"}}" http://localhost:5000/processes/k8s-process/execution
+
+from ftplib import FTP
+import logging, time
+import yaml
+import fsspec
+from pygeoapi.process.base import BaseProcessor, ProcessorExecuteError
+import os
+import s3fs
+# from climate_eed import fetch_var_smhi
+import datetime
+import xarray as xr
+
+LOGGER = logging.getLogger(__name__)
+
+#: Process metadata and description
+PROCESS_METADATA = {
+    'version': '0.2.0',
+    'id': 'ingestor-process',
+    'title': {
+        'en': 'Ingestor Process',
+    },
+    'description': {
+        'en': 'Ingestor Process is a process that fetches data from an FTP server and stores it in a Zarr file in an S3 bucket.'
+          'The process is used to ingest data from the SMHI FTP server and store it in an S3 bucket.' 
+          'The process requires the following inputs: issue_date, data_dir, living_lab, zarr_out.' 
+          'The process fetches the data from the FTP server, reads the NetCDF files, and stores the data in a Zarr file in an S3 bucket.' 
+          'The process returns the URL of the Zarr file in the S3 bucket.' 
+          'The process also updates the pygeoapi configuration file to include the new dataset.'},
+    'jobControlOptions': ['sync-execute', 'async-execute'],
+    'keywords': ['ingestor process'],
+    'links': [{
+        'type': 'text/html',
+        'rel': 'about',
+        'title': 'information',
+        'href': 'https://example.org/process',
+        'hreflang': 'en-US'
+    }],
+    'inputs': {
+        'issue_date': {
+            'title': 'Issue Date',
+            'description': 'The issue date of the forecast',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'data_dir': {
+            'title': 'Data Directory',
+            'description': 'The directory where the data is stored',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'living_lab': {
+            'title': 'Living Lab',
+            'description': 'The living lab for which the data is fetched',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'zarr_out': {
+            'title': 'Zarr Output',
+            'description': 'The URL of the Zarr file in the S3 bucket',
+            'schema': {
+                'type': 'string'
+            }
+        }
+    },
+    'outputs': {
+        'id': {
+            'title': 'ID',
+            'description': 'The ID of the process execution',
+            'schema': {
+                'type': 'string'
+            }
+        },
+        'value': {
+            'title': 'Value',
+            'description': 'The URL of the Zarr file in the S3 bucket',
+            'schema': {
+                'type': 'string'
+            }
+        }
+    },
+    'example': {
+        "inputs": {
+            "issue_date": "202404",
+            "data_dir": "seasonal_forecast",
+            "living_lab": "georgia"
+        }
+    }
+}
+
+
+
+def download_files_from_ftp(ftp, folder):
+    # ftp.cwd(folder)
+    files = ftp.nlst()
+    nc_files = []
+    for file in files:
+        if file.endswith('.nc'):
+            local_filename = os.path.join(f"./seasonal_forecast/{folder}", file)
+            nc_files.append(local_filename)
+            if not os.path.exists(local_filename):
+
+                if not os.path.exists(f"./seasonal_forecast/{folder}"):
+                    os.makedirs(f"./seasonal_forecast/{folder}")
+                with open(local_filename, 'wb') as f:
+                    ftp.retrbinary('RETR ' + file, f.write)
+                # print(f"Downloaded: {local_filename}")
+            else:
+                # print(f"File already exists: {local_filename}")
+                pass
+    ftp.cwd("..")
+    return nc_files
+
+# Function to read geometry and bbox from NetCDF file
+def read_netcdf(file_path):
+    nc_file = fsspec.open(file_path,anon=True)
+    nc = xr.open_dataset(nc_file.open())  # Dataset(file_path, 'r')
+    # Extract geometry and bbox from the NetCDF file
+
+    return nc
+
+
+class IngestorProcessProcessor(BaseProcessor):
+    """
+    Ingestor Processor example
+    """
+
+    def __init__(self, processor_def):
+        """
+        Initialize object
+
+        :param processor_def: provider definition
+
+        :returns: pygeoapi.process.ingestor_process.IngestorProcessProcessor
+        """
+
+        super().__init__(processor_def, PROCESS_METADATA)
+
+    def execute(self, data):
+
+        mimetype = 'application/json'
+
+        data_dir = data.get('data_dir')
+        issue_date = data.get('issue_date')
+        living_lab = data.get('living_lab')
+        zarr_out = data.get('zarr_out')
+        ftp_config= {
+            "url": os.environ.get("FTP_HOST"),
+            "folder": os.environ.get("FTP_DIR"),
+            "user": os.environ.get("FTP_USER"),
+            "passwd": os.environ.get("FTP_PASS")
+        }
+
+        if issue_date is None:
+            raise ProcessorExecuteError('Cannot process without a issue_date')
+        if data_dir is None:
+            raise ProcessorExecuteError('Cannot process without a data_dir')
+        if living_lab is None:
+            raise ProcessorExecuteError('Cannot process without a living_lab')
+        # if zarr_out is None:
+        #     raise ProcessorExecuteError('Cannot process without a zarr_out')
+        
+        # data = fetch_var_smhi(living_lab=living_lab, data_dir=data_dir, issue_date=issue_date,ftp_config=ftp_config)  #(collections=collections,repository="smhi")
+
+        data_array = []
+
+        # Connect to FTP server
+        ftp = FTP(ftp_config['url'])
+        ftp.login(user=ftp_config['user'], passwd=ftp_config['passwd'])
+
+        remote_folder = f"{ftp_config['folder']}/{living_lab}/{data_dir}/{issue_date}"
+        local_folder = f"./{data_dir}/{issue_date}"
+
+        with FTP(ftp_config['url']) as ftp:
+            ftp.login(ftp_config['user'], ftp_config['passwd'])
+            ftp.cwd(remote_folder)
+            
+            files = download_files_from_ftp(ftp, remote_folder)
+
+        for file_nc in files:
+            file_nc_path = f"{local_folder}/{file_nc}"
+            data = read_netcdf(file_nc)
+
+            model = file_nc_path.split('COUT_')[1].split('.')[0]
+            data.coords['model'] = model
+
+            if "COUT" in data:
+                data["COUT"] = data["COUT"].expand_dims('model')
+
+            data_array.append(data)
+
+        data = xr.merge(data_array)
+
+
+                
+        # for each value in model coordinate create new variable with the model name
+        for model in data['model'].values:
+            data[f"COUT_{model}"] = data['COUT'].where(data['model'] == model)
+            data[f"COUT_{model}"].attrs['long_name'] = f"COUT_{model}"
+
+
+        # drop the original COUT variable and model variable
+
+
+        df = data.to_dataframe()
+        print("Original DataFrame:\n", df)
+
+        # Reset the index to convert the MultiIndex into columns
+        df_reset = df.reset_index()
+        df_reset = df_reset.drop(columns=['geo_z','COUT','model'])
+
+        print("NEW DataFrame:\n", df_reset)
+
+        # print("RESETTED DataFrame:\n", df_reset)
+        # Set 'geo_x' and 'geo_y' as part of the new index and drop 'id'
+        df_reindexed = df_reset.set_index(['geo_x', 'geo_y', 'id','time'])
+
+        print("Reindexed DataFrame:\n", df_reindexed)
+
+        df_reindexed = df_reindexed.stack().dropna().unstack()
+
+        print("Transformed DataFrame:\n", df_reindexed)
+
+        # apply id value to all the columns adding a new variable with id as name
+        # df_reindexed['id_var'] = df_reindexed.index.get_level_values('id')
+
+        # drop id from the index
+        df_reindexed = df_reindexed.reset_index().set_index(['geo_x', 'geo_y', 'time'])
+
+        print("Transformed DataFrame 2:\n", df_reindexed)
+
+        #####################
+
+        # convert the dataframe to xarray dataset
+        data = xr.Dataset.from_dataframe(df_reindexed)
+
+        # Select the first time slice
+        id_var = data['id'].isel(time=0)
+
+        # Drop the time dimension
+        id_var = id_var.drop('time')
+
+        # Assign the modified variable back to the dataset
+        data['id'] = id_var
+
+
+        # for each value in model coordinate create new variable with the model name
+        for var in data.variables:
+            if var.startswith('COUT'):
+                data[var].attrs['long_name'] = var
+
+        # data["COUT"].attrs['long_name'] = "Discharge"
+        print("DATA")
+        print(data.values)
+
+        print("***********************************")
+        s3 = s3fs.S3FileSystem(anon=True)
+        if zarr_out:
+            remote_url = zarr_out
+            # Check if the path already exists
+            if s3.exists(remote_url):
+                raise ProcessorExecuteError(f'Path {remote_url} already exists')
+        else:    
+            bucket_name = os.environ.get("DEFAULT_BUCKET")
+            remote_path = os.environ.get("DEFAULT_REMOTE_DIR")
+            remote_url = f's3://{bucket_name}/{remote_path}dataset_smhi_{int(datetime.datetime.now().timestamp())}.zarr'
+
+        store= s3fs.S3Map(root=remote_url, s3=s3, check=False)
+
+        data.attrs['long_name'] = "seasonal_forecast"
+        data.to_zarr(store=store,
+                            consolidated=True,
+                            mode='w')
+        
+        # get min/max values for geo_x, geo_y and time
+        min_x = float(data['geo_x'].min().values)
+        max_x = float(data['geo_x'].max().values)
+        min_y = float(data['geo_y'].min().values)
+        max_y = float(data['geo_y'].max().values)
+
+        min_time = data['time'].min().values
+        max_time = data['time'].max().values
+        
+        # convert np.datetime64 to datetime object 
+        datetime_max = datetime.datetime.fromtimestamp(max_time.tolist()/1e9,tz=datetime.timezone.utc)
+        datetime_min = datetime.datetime.fromtimestamp(min_time.tolist()/1e9,tz=datetime.timezone.utc)
+
+        print(f"min_x: {min_x} T {type(min_x)}, max_x: {max_x} T {type(max_x)}")
+        print(f"min_y: {min_y} T {type(min_y)}, max_y: {max_y} T {type(max_y)}")
+        print(f"min_time: {datetime_min} T {type(datetime_min)}, max_time: {datetime_max} T {type(datetime_max)}")
+
+
+        with open('/pygeoapi/local.config.yml', 'r') as file:
+            config = yaml.safe_load(file)
+
+        config['resources'][f'georgia_seasonal_forecast_{issue_date}'] = {
+            'type': 'collection',
+            'title': f'georgia_seasonal_forecast_{issue_date}',
+            'description': 'SMHI Discharge data of Georgia',
+            'keywords': ['Georgia', 'country'],
+            'extents': {
+                'spatial': {
+                    'bbox': [min_x, min_y, max_x, max_y],
+                    'crs': 'http://www.opengis.net/def/crs/OGC/1.3/CRS84'
+                },
+                'temporal': {
+                    'begin': datetime_min,         # datetime.datetime(2024, 4, 1, 0, 0),
+                    'end': datetime_max            # datetime.datetime(2024, 4, 30, 0, 0)       
+                    }
+                },                      
+            'providers': [
+                {
+                    'type': 'edr',
+                    'name': 'xarray-edr',
+                    'data': remote_url,
+                    'x_field': 'geo_x',
+                    'y_field': 'geo_y',
+                    'time_field': 'time',
+                    'format': {'name': 'zarr', 'mimetype': 'application/zip'},
+                    'options': {
+                        's3': {'anon': True, 'requester_pays': False}
+                    }
+                }
+            ]
+        }
+
+        print("***********************************")
+        print(config['resources'][f'georgia_seasonal_forecast_{issue_date}'])
+        print("***********************************")
+
+        with  open('/pygeoapi/local.config.yml', 'w') as outfile:
+            yaml.dump(config, outfile, default_flow_style=False)
+
+
+        outputs = {
+            'id': 'ingestor-process',
+            'value': remote_url
+        }
+        return mimetype, outputs
+
+    def __repr__(self):
+        return f'<IngestorProcessProcessor> {self.name}'
+
