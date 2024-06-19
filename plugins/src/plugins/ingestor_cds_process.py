d6bb2645e21afaaf9a1920ec323ad4a4fa12f850
EHJ-52n <e.h.juerrens@52north.org>
2024-06-19 17:26:29 +0200

    Fix logging in pygeoapi context

--- plugins/src/plugins/ingestor_cds_process.py
+++ plugins/src/plugins/ingestor_cds_process.py
@@ -50,13 +50,6 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 import logging
 import sys
 
-logging.basicConfig(
-    format="[%(levelname)s] %(asctime)s %(message)s",
-    level=logging.DEBUG,
-    stream=sys.stdout)
-logger = logging.getLogger(__name__)
-
-
 LOGGER = logging.getLogger(__name__)
 load_dotenv(find_dotenv())
 #: Process metadata and description
@@ -287,12 +280,12 @@ class IngestorCDSProcessProcessor(BaseProcessor):
                         out_data = future.result()
                         datasets.append(out_data)
                     except Exception as exc:
-                        logger.error(f"{day} generated an exception: {exc}")
+                        LOGGER.error(f"{day} generated an exception: {exc}")
             data = xr.concat(datasets,dim='time')
         else:
             # check if year, month and day are in the query
             if 'year' in query and 'month' in query and 'day' in query:
-                logger.debug('Fetching data for a specific date', query['year'], query['month'], query['day'])
+                LOGGER.debug('Fetching data for a specific date', query['year'], query['month'], query['day'])
             else:
                 query['year'] = str(datetime.now().year)
                 query['month'] = str(datetime.now().month)
