0d2f6da81c30511c8786fd5e9dbb7e5578c5f1a8
JohannesSchnell <j.schnell@52north.org>
2024-09-10 13:47:14 +0200

    refactor

---
+++ src/invoke/utils.py
@@ -0,0 +1,38 @@
+import os
+import json
+import requests
+import time
+
+def invoke_ingestor_process(execute_url, data, logger):
+    success = False
+    n_tries = 0
+    max_tries = 5
+    while not success and n_tries < max_tries:
+        n_tries += 1
+        logger.debug(f"[{n_tries}/{max_tries}]: Send POST to '{execute_url}")
+        try:
+            response = requests.post(execute_url,
+                headers={
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                data=json.dumps(data)
+            )
+            logger.debug(f"Response status code: {response.status_code}")
+            logger.debug(f"Response body       : {response.text}")
+
+            if response.status_code >= 200 and response.status_code < 300:
+                success = True
+
+            elif n_tries < max_tries:
+                logger.info("Retrying in 10 seconds...")
+                time.sleep(10)
+
+            else:
+                logger.error(f"Failed to ingest creaf data. Stopped after {max_tries} retries")
+
+        except Exception as e:
+            logger.error(f"Try #{n_tries}. Failed to invoke the ingestor: {e}")
+            logger.error("Retrying in 10 seconds...")
+
+            time.sleep(10)
