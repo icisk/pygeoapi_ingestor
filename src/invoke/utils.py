a214bb8c5d66db0ffffb16bbc4bdd5b8694b844e
EHJ-52n <e.h.juerrens@52north.org>
2025-01-08 15:34:56 +0100

    Improve invoke loop...
    - Make retries configurable via env variable INVOKE_MAX_TRIES
    
    - Make intermediate sleep configurable via env variable INVOKE_SLEEP_SECONDS

--- src/invoke/utils.py
+++ src/invoke/utils.py
@@ -1,4 +1,5 @@
 import json
+import os
 import requests
 import sys
 import time
@@ -6,7 +7,9 @@ import time
 def invoke_ingestor_process(execute_url, data, logger):
     success = False
     n_tries = 0
-    max_tries = 5
+    max_tries = int(os.getenv('INVOKE_MAX_TRIES', 5))
+    sleep_seconds = int(os.getenv('INVOKE_SLEEP_SECONDS', 10))
+    logger.info(f"Starting execution trigger with '{max_tries}' max tries and '{sleep_seconds}'s of intermediate sleep.")
     while not success and n_tries < max_tries:
         n_tries += 1
         logger.info(f"[{n_tries}/{max_tries}]: Send POST to '{execute_url}")
@@ -19,22 +22,26 @@ def invoke_ingestor_process(execute_url, data, logger):
                 data=json.dumps(data)
             )
             logger.info(f"Response status code: {response.status_code}")
-            logger.info(f"Response body       : {response.text}")
+            logger.info("Response body       :")
+            logger.info("---------------------")
+            logger.info(f"{response.text}")
+            logger.info("---------------------")
 
             if response.status_code >= 200 and response.status_code < 300:
                 #TODO next iteration: for long running tasks implement async processes and use /jobs/<job-id>
                 success = True
 
             elif n_tries < max_tries:
-                logger.info("Retrying in 10 seconds...")
-                time.sleep(10)
+                #TODO add check for response code, hence we retry not for all errors, but only specific ones
+                logger.info(f"Retrying in {sleep_seconds}s seconds...")
+                time.sleep(sleep_seconds)
 
             else:
-                logger.error(f"Failed to ingest creaf data. Stopped after {max_tries} retries")
+                logger.error(f"Failed to ingest data. Stopped after '{max_tries}' retries")
                 sys.exit(52)
 
         except Exception as e:
             logger.error(f"Try #{n_tries}. Failed to invoke the ingestor: {e}")
-            logger.error("Retrying in 10 seconds...")
+            logger.error(f"Retrying in {sleep_seconds}s...")
 
-            time.sleep(10)
+            time.sleep(sleep_seconds)
