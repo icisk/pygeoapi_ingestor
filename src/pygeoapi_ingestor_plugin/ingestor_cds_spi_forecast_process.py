e92347217eb82ebed7811bdfeb3252f037a2eb86
PimpMyGit <tommasoredaelli276@gmail.com>
2025-03-21 17:07:25 +0100

    add creation of spi forecast zonal stats collection (geojson) . zonal stats avaliable are ensemble, min, max, perc-05-10-30-70-90-95

--- src/pygeoapi_ingestor_plugin/ingestor_cds_spi_forecast_process.py
+++ src/pygeoapi_ingestor_plugin/ingestor_cds_spi_forecast_process.py
@@ -111,9 +111,15 @@ PROCESS_METADATA = {
             'schema': {
             }
         },
-        'spi_coverage_s3_uri': {
+        'spi_coverage_collection_s3_uri': {
             'title': 'SPI coverage S3 URI',
             'description': 'SPI coverage S3 URI',
+            'schema': {
+            }
+        },
+        'spi_coverage_collection_id': {
+            'title': 'SPI coverage collection ID',
+            'description': 'SPI coverage collection PyGeoApi ID',
             'schema': {
             }
         },
@@ -303,11 +309,19 @@ class IngestorCDSSPIForecastProcessProcessor(BaseProcessor):
         ds = build_data(spi_ts, periods_of_interest, month_spi_coverages)
         collection_params = spi_utils.create_s3_collection_data(living_lab, ds, data_type='forecast')
         spi_utils.update_config(living_lab, collection_params)
-        collection_info = {
-            'collection_id': collection_params['pygeoapi_id'],
-            'collection_s3_uri': collection_params['s3_uri']
-        }
-        return ds, collection_info
+        return ds, collection_params
+    
+    
+    def save_spi_basin_zonal_stats_to_collection(self, living_lab, spi_dataset, spi_coverage_collection_params):
+        ds_zonal_stats = spi_utils.compute_zonal_stats(living_lab, spi_dataset)
+        if ds_zonal_stats is not None:    
+            collection_params = spi_utils.create_s3_zonal_stats_collection_data(living_lab, ds_zonal_stats, spi_coverage_collection_params, data_type='forecast')
+            spi_utils.update_config(living_lab, collection_params)
+            return ds_zonal_stats, collection_params
+        else:
+            LOGGER.debug('Zonal stats not computed due to missing basin geojson')
+            return None
+        
 
 
     def execute(self, data):
@@ -326,7 +340,18 @@ class IngestorCDSSPIForecastProcessProcessor(BaseProcessor):
             periods_of_interest, month_spi_coverages = self.compute_coverage_spi(ref_dataset, poi_dataset, spi_ts)
 
             # Save SPI coverage to collection
-            ds, collection_info = self.save_spi_coverage_to_collection(living_lab, spi_ts, periods_of_interest, month_spi_coverages)
+            ds, spi_coverage_collection_params = self.save_spi_coverage_to_collection(living_lab, spi_ts, periods_of_interest, month_spi_coverages)
+            
+            # Compute SPI basin-zonal-stats and save to collection
+            zonal_stats_out = self.save_spi_basin_zonal_stats_to_collection(living_lab, ds, spi_coverage_collection_params)
+            ds_zonal_stats, spi_zonal_stats_collection_params = zonal_stats_out if zonal_stats_out is not None else (None, None)
+            if spi_zonal_stats_collection_params is not None:
+                spi_zonal_stats_collection_info = {
+                    'spi_zonal_stats_collection_id': spi_zonal_stats_collection_params['pygeoapi_id'],
+                    'spi_zonal_stats_collection_s3_uri': spi_zonal_stats_collection_params['s3_uri'],
+                }
+            else:
+                spi_zonal_stats_collection_info = {}
 
             # Convert SPI coverage in the requested output format
             output_data = {}
@@ -338,10 +363,18 @@ class IngestorCDSSPIForecastProcessProcessor(BaseProcessor):
                         'collection_data': out_spi_coverages
                     }
                 }
-
+                if ds_zonal_stats is not None:
+                    output_data['output_data']['zonal_stats'] = ds_zonal_stats.to_geo_dict()
+            
+            # Prepare response
             outputs = {
                 'status' : 'OK',
-                ** collection_info,
+                
+                'spi_coverage_collection_id': spi_coverage_collection_params['pygeoapi_id'],
+                'spi_coverage_collection_s3_uri': spi_coverage_collection_params['s3_uri'],
+                
+                ** spi_zonal_stats_collection_info,
+                
                 ** output_data
             }
 
